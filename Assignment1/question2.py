a55c9ea3a60549dbaa27528ec848e41f76aacc78
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-09-16 22:41:02 +0530

    perform arathmatic opration

---
+++ Assignment1/question2.py
@@ -0,0 +1,10 @@
+# Q2. Take two numbers as input from the user and print their sum, difference, 
+# product, and quotient. 
+
+a = int(input("Enter the value of a:"))
+b = int(input("Enter the value of b:"))
+
+print("The sum of these two number is:" + str(a + b))
+print("The difference of these two number is :" + str(a - b))
+print("product of these two number is : " + str(a * b))
+print("quotient of these two number is :" + str(a / b))
