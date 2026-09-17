ecd07c120b275c2946343d8a8f8b6285e87f1b9b
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-09-17 20:55:40 +0530

    print user value in different datatype and print value and it's type

--- Assignment1/question3.py
+++ Assignment1/question3.py
@@ -1,8 +1,8 @@
-# Q3. Ask the user to enter two integers and one float. Convert them all to floats 
-# and print their average. 
+# Q3. Ask the user to enter two integers and one float. Convert them all to floats
+# and print their average.
 
-a = int(input("Enter the value of a:"))
-b = int(input("Enter the value of b:"))
-c = float(input("Enter the value of c:"))
-sum = (a + b + c)/3
-print("The average of these three number is :" + str(float(sum)))
+a = float(input("Enter the value of a: "))
+b = float(input("Enter the value of b: "))
+c = float(input("Enter the value of c: "))
+average = (a + b + c) / 3
+print(f"The average of these three numbers is: {average}")
---
+++ Assignment1/question4.py
@@ -0,0 +1,11 @@
+# Q4. The user enters a string containing a number (e.g."45" ). Convert it to: 
+# • a string again 
+# • an integer 
+# • a float
+# Print all three values with their types. 
+
+a = input("Enter the string value:")
+print("String:", a, type(a))
+print("Integer:", int(a), type(int(a)))
+print("Float:", float(a), type(float(a)))
+
