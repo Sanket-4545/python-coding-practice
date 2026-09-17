011eb622fd4340ae7e7a79ca8aa3f56e5332a2fc
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-09-17 21:18:31 +0530

    Conversion of themprature form celsius to Fahrenheit

---
+++ Assignment1/question7.py
@@ -0,0 +1,10 @@
+# Q7. Ask the user for a temperature in Celsius (string input). Convert it to , 
+# then calculate and print temperature in Fahrenheit. 
+# float 
+# Conversion formula: FahrenheitTemp = (CelsiusTemp ∗ (9/5)) + 
+# 32 
+
+Celsius = float(input("Enter the temperature in Celsius: "))
+FahrenheitTemp = (Celsius * (9/5)) + 32
+print("Temperature in Fahrenheit:", FahrenheitTemp)
+ 
