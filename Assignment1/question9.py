5b9034a9f27afde5456ed3e94ffcc00ee3fd1755
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-09-17 21:40:55 +0530

    To find SI

---
+++ Assignment1/question9.py
@@ -0,0 +1,10 @@
+# Q9. Ask the user for: Principal (P), Rate (R), Time (T). Convert all to and 
+# compute simple interest: 
+# SI = (P ∗ R ∗ T )/100 
+
+
+P = float(input("Enter the value of principal:"))
+R = float(input("Enter the value of rate:"))
+T = float(input("Enter the value of Time:"))
+SI = (P * R * T )/100
+print(SI)
