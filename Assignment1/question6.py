8d04498564bb32394623405a4f34dfa64dacd28a
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-09-17 21:06:01 +0530

    swap two number

---
+++ Assignment1/question6.py
@@ -0,0 +1,8 @@
+# Q6. Write a program to swap values of two numbers entered by the user.
+a = input("Enter the value of a :")
+b = input("Enter the value of b :")
+
+temp = a 
+a = b 
+b = temp
+print (a, b)
