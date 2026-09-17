c1971bfc7c6353b702072da3c2514750ebf5e9e4
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-09-17 20:35:54 +0530

    Average of two int and one float number

---
+++ Assignment1/question3.py
@@ -0,0 +1,8 @@
+# Q3. Ask the user to enter two integers and one float. Convert them all to floats 
+# and print their average. 
+
+a = int(input("Enter the value of a:"))
+b = int(input("Enter the value of b:"))
+c = float(input("Enter the value of c:"))
+sum = (a + b + c)/3
+print("The average of these three number is :" + str(float(sum)))
