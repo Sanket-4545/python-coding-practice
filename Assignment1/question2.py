484b3deae5cab9aa68743c265243457ed31271e6
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-09-17 20:58:43 +0530

    just change minor change

--- Assignment1/question2.py
+++ Assignment1/question2.py
@@ -1,10 +1,14 @@
 # Q2. Take two numbers as input from the user and print their sum, difference, 
 # product, and quotient. 
 
-a = int(input("Enter the value of a:"))
-b = int(input("Enter the value of b:"))
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
 
-print("The sum of these two number is:" + str(a + b))
-print("The difference of these two number is :" + str(a - b))
-print("product of these two number is : " + str(a * b))
-print("quotient of these two number is :" + str(a / b))
+print("The sum of these two numbers is:", a + b)
+print("The difference of these two numbers is:", a - b)
+print("The product of these two numbers is:", a * b)
+
+if b != 0:
+    print("The quotient of these two numbers is:", a / b)
+else:
+    print("Quotient is not possible because division by zero is not allowed.")
--- Assignment1/question3.py
+++ Assignment1/question3.py
@@ -5,4 +5,4 @@ a = float(input("Enter the value of a: "))
 b = float(input("Enter the value of b: "))
 c = float(input("Enter the value of c: "))
 average = (a + b + c) / 3
-print(f"The average of these three numbers is: {average}")
+print("The average of these three numbers is:", average)
