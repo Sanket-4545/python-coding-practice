53ed48247fabe86723ab3a6496c009f69d2ec407
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-09-17 22:53:12 +0530

    admin authentication

---
+++ Assignmen2/admin_authentication.py
@@ -0,0 +1,6 @@
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+if username == "admin" and password == "pass":
+    print("Login successful")
+else:
+    print("Login failed. Please try again.......")
