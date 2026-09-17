46b43f1452a03628f52a0271515b3c21329750f0
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-09-17 22:46:25 +0530

    age classificatino

---
+++ Assignmen2/age_classification.py
@@ -0,0 +1,10 @@
+age = int(input("Enter the value of your age:"))
+
+if age < 13:
+    print("You are a child")
+
+elif age >= 13 and age < 18:
+    print("You are a teenager")
+
+else:
+    print("You are an adult")
